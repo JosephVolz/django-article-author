7b056d372d16b57242a0861848a6652c206d24c3
Joseph Volz <volzjoseph715@gmail.com>
2023-10-24 02:26:58 -0700

    Add authors test

--- techtest/authors/tests.py
+++ techtest/authors/tests.py
@@ -1,3 +1,98 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from techtest.authors.models import Author
+
+
+class AuthorListViewTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse("authors-list")
+        self.author_1 = Author.objects.create(first_name="Joe", last_name="Doe")
+        self.author_2 = Author.objects.create(first_name="Max", last_name="Luis")
+
+    def test_serializes_with_correct_data_shape_and_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertCountEqual(
+            response.json(),
+            [
+                {
+                    "id": self.author_1.id,
+                    "first_name": "Joe",
+                    "last_name": "Doe",
+                },
+                {
+                    "id": self.author_2.id,
+                    "first_name": "Max",
+                    "last_name": "Luis",
+                },
+            ],
+        )
+
+    def test_creates_new_author(self):
+        payload = {
+            "first_name": "Joe",
+            "last_name": "Doe",
+        }
+        response = self.client.post(
+            self.url, data=json.dumps(payload), content_type="application/json"
+        )
+        author = Author.objects.last()
+        self.assertEqual(response.status_code, 201)
+        self.assertIsNotNone(author)
+        self.assertEqual(Author.objects.count(), 3)
+        self.assertDictEqual(
+            {
+                "id": author.id,
+                "first_name": "Joe",
+                "last_name": "Doe",
+            },
+            response.json(),
+        )
+
+
+class AuthorViewTestCase(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(code="AL", name="Albania")
+        self.url = reverse("author", kwargs={"author_id": self.author.id})
+
+    def test_serializes_single_record_with_correct_data_shape_and_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertCountEqual(
+            response.json(),
+            {
+                "id": self.author.id,
+                "code": "AL",
+                "name": "Albania",
+            },
+        )
+
+    def test_updates_author(self):
+        payload = {
+            "id": self.author.id,
+            "code": "US",
+            "name": "United States of America",
+        }
+        response = self.client.put(
+            self.url, data=json.dumps(payload), content_type="application/json"
+        )
+        author = Author.objects.filter(id=self.author.id).first()
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(author)
+        self.assertEqual(Author.objects.count(), 1)
+        self.assertDictEqual(
+            {
+                "id": author.id,
+                "code": "US",
+                "name": "United States of America",
+            },
+            response.json(),
+        )
+
+    def test_removes_author(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Author.objects.count(), 0)
